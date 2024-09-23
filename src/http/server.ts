a26b6e7ea0565dec0a48b233deece475b2c58017
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2024-09-23 10:34:44 -0300

    feat: add get month receipt route

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,7 @@ import { cancelOrder } from '@routes/cancel-order'
 import { deliverOrder } from '@routes/deliver-order'
 import { dispatchOrder } from '@routes/dispatch-order'
 import { getManagedRestaurante } from '@routes/get-managed-restaurant'
+import { getMonthReceipt } from '@routes/get-month.receipt'
 import { getOrderDetails } from '@routes/get-order-details'
 import { getOrders } from '@routes/get-orders'
 import { getProfile } from '@routes/get-profile'
@@ -50,6 +51,7 @@ const app = new Elysia()
   .use(cancelOrder)
   .use(deliverOrder)
   .use(dispatchOrder)
+  .use(getMonthReceipt)
 
 app.listen(3333, () => {
   console.log(`Server running at http://localhost:3333`)
