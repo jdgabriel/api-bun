40cbccf799d93d185d1f0fa77bef56ba94358e0b
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2024-09-23 10:26:30 -0300

    chore: add new routes on server

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,9 @@ import { Elysia, t } from 'elysia'
 
 import { env } from '@env'
 import { approveOrder } from '@routes/approve-order'
+import { cancelOrder } from '@routes/cancel-order'
+import { deliverOrder } from '@routes/deliver-order'
+import { dispatchOrder } from '@routes/dispatch-order'
 import { getManagedRestaurante } from '@routes/get-managed-restaurant'
 import { getOrderDetails } from '@routes/get-order-details'
 import { getProfile } from '@routes/get-profile'
@@ -39,6 +42,9 @@ const app = new Elysia()
   .use(registerRestaurante)
   .use(getOrderDetails)
   .use(approveOrder)
+  .use(cancelOrder)
+  .use(deliverOrder)
+  .use(dispatchOrder)
 
 app.listen(3333, () => {
   console.log(`Server running at http://localhost:3333`)
