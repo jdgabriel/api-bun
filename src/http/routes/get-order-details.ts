a3fe610d137db29fca788565a17a6c8ecac85c1f
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2024-09-23 10:48:31 -0300

    chore: add restaurant identification in sql database

--- src/http/routes/get-order-details.ts
+++ src/http/routes/get-order-details.ts
@@ -45,8 +45,11 @@ export const getOrderDetails = new Elysia().use(auth).get(
           },
         },
       },
-      where(fields, { eq }) {
-        return eq(fields.id, orderId)
+      where(fields, { eq, and }) {
+        return and(
+          eq(fields.id, orderId),
+          eq(fields.restaurantId, restauranteId),
+        )
       },
     })
 
