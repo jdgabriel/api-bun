91a1b7f0516090845c8ac1021dd8c7106c22b5bb
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2024-09-23 09:32:38 -0300

    feat: initial setup jwt

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 
+import cookie from '@elysiajs/cookie'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
 import { registerRestaurante } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurante).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restauranteId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurante)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log(`Server running at http://localhost:3333`)
